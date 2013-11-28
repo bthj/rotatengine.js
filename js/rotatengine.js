a818b98342177cf10e62329ff087a107eee30df1
Björn Þór Jónsson <bangsi@bthj.is>
2013-11-28 15:27:42 +0100

    Size of items dependent on viewport.  Fit to viewport on resize.

--- js/rotatengine.js
+++ js/rotatengine.js
@@ -42,26 +42,9 @@ Rotatengine.prototype = {
         }
         return itemElement;
     },
-//    rotateElementsOnACircle: function( viewRotation ) {
-//        var self = this;
-//        this.items.each(function(i){
-//            var thisItemRadians = (self.radiansPerItem * i) + viewRotation;
-//            
-//            $(this).css({"transform": ""});
-//        });
-//    },
     spreadElementsOnACircle: function( viewRotation ) {
         var self = this;
-//        if( undefined === centerElement ) {
-//            var items = $(container).find('.rotatem');
-//            centerElement = items.eq(Math.floor(items.length/2));
-//        }
-//        
-//        var leftElements = centerElement.prevAll('.rotatem');
-//        var rightElements = centerElement.nextAll('.rotatem');
-        
-//        var transform_center = "translateX(0px) rotateY(0deg) translateZ(0)";
-//        centerElement.css({"transform": transform_center});
+
         
         var perspective = this.viewWidth / 2;
         
@@ -69,15 +52,19 @@ Rotatengine.prototype = {
         
         self.container.css({"transform": "perspective("+perspective+")"});
         
+        var precision = 1000000000000000; // here 16 significant numbers, max 18 ?
+        
         this.items.each(function(i){
-            var thisItemRadians = (self.radiansPerItem * i) + viewRotation;
+            // let's go clockwise, thus self.fullCircleRadins - ...
+            var thisItemRadians = 
+                self.fullCircleRadians - (self.radiansPerItem * i) + viewRotation;
 //            var thisItemRadians = (self.degreesPerItem * i) + viewRotation;
             // http://javascript.info/tutorial/number-math#rounding-to-given-precision
-            var x = (Math.round( Math.cos(thisItemRadians) * 100 ) / 100) * radius;
-            var z = -( (Math.round( Math.sin(thisItemRadians) * 100 ) / 100) * radius );
-            if( i == 0 ) {
+            var x = (Math.round( Math.cos(thisItemRadians) * precision ) / precision) * radius;
+            var z = -( (Math.round( Math.sin(thisItemRadians) * precision ) / precision) * radius );
+//            if( i == 0 ) {
 //                console.log( $(this).find('span').text() + " # x: " + x + ", z: " + z + ", rota: " + thisItemRadians );
-            }
+//            }
             var transform =  
                 "perspective("+ (perspective) +") " +
                 "translateZ("+(z + radius)+"px) " + 
@@ -88,34 +75,6 @@ Rotatengine.prototype = {
                 //"rotateY("+(thisItemRadians)+"deg)";
             $(this).css({"transform": transform});
         });
-        
-        
-//        var degreePerItem = Math.floor(360/items.length);
-//        var xTranslationPerItem = Math.floor(2000/items.length);
-        
-//        var leftCompensation = -60;
-//        leftElements.each(function(i){
-//            leftCompensation += 60;
-//            var itemDelta = i+1;
-//            var rotationRadians = radiansPerItem * itemDelta;
-//            var transform_left = "translateX("+((itemDelta)*-250+leftCompensation)+"px) rotateY("+(rotationRadians)+"rad) translateZ("+((itemDelta*xTranslationPerItem))+"px)";
-//            $(this).css({"transform": transform_left});
-////            if( rotationDegrees > 90 ) {
-////                $(this).hide();
-////            }
-//        });
-        
-//        var rightCompensation = -60;
-//        rightElements.each(function(i) {
-//            rightCompensation += 60;
-//            var itemDelta = i+1;
-//            var rotationRadians = radiansPerItem * itemDelta;
-//            var transform_right = "translateX("+((itemDelta)*200+rightCompensation)+"px) rotateY(-"+(rotationRadians)+"rad) translateZ("+((itemDelta*xTranslationPerItem))+"px)";
-//            $(this).css({"transform": transform_right});
-////            if( rotationDegrees > 90 ) {
-////                $(this).hide();
-////            }
-//        });
     },
     mouseDown: function( a ) {
         this.isMouseDown = true;
@@ -142,17 +101,31 @@ Rotatengine.prototype = {
             this.currentY = this.lastY;
         }
     },
+    getViewRotation: function() {
+        return this.viewRotation;
+    },
+    fitToView: function() {
+        this.viewWidth = $(window).width() / 1.023;
+        this.viewHeight = $(window).height() / 1.023;
+        
+        this.container.find(".rotatem .textItem").each(function(){
+            // http://www.sitepoint.com/new-css3-relative-font-size/
+            // 1vw is 1% of the viewport width 
+            // 1vh is 1% of the viewport height
+            // 1vmin is the smallest of 1vw and 1vh
+            $(this).css("font-size", "35vmin");
+        });
+        
+        this.spreadElementsOnACircle( this.viewRotation );
+        
+        this.container.width(this.viewWidth).height(this.viewHeight)
+                .css("overflow", "hidden");
+    },
     initialize: function( levelNumber ) {
         var self = this;
         var level = this.config.levels[levelNumber];
         
         self.container = $('#rotaspace');
-        
-        self.viewWidth = $(window).width() / 1.023;
-        self.viewHeight = $(window).height() / 1.023;
-        
-        
-        //rotaSpace.width(this.viewWidth).height(this.viewHeight).css("overflow", "hidden");
         
         level.items.forEach(function(item){
             
@@ -167,9 +140,9 @@ Rotatengine.prototype = {
         self.radiansPerItem = self.fullCircleRadians / self.items.length;
 //        self.degreesPerItem = 360 / self.items.length;
         
-        self.spreadElementsOnACircle( 0 );
+        self.viewRotation = Math.PI/2;
         
-        self.container.width(self.viewWidth).height(self.viewHeight).css("overflow", "hidden");
+        self.fitToView();
         
         self.rotationPoller = setInterval( function(){self.rotateByMouseMoveDelta()}, 50 );
     }
